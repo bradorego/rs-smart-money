089751d56c9fc795830f9272a9155a8b677283fc
Brad Orego <me@bradorego.com>
2017-08-31 17:02:54 -0500

    adding express; making smash.js an express app

--- smash.js
+++ smash.js
@@ -1,3 +1,8 @@
+var express = require("express");
+var app = express();
+var compression = require("compression");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
 var firebase = require("firebase");
 firebase.initializeApp({
   apiKey: "AIzaSyBjwJEBgAq8iHdkYhXXIOCLuV172agVQ7Y",
@@ -10,36 +15,56 @@ firebase.initializeApp({
 var itemRef = firebase.database().ref("items");
 var rsapi = require('runescape-api');
 
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('www'));
+app.use(compression());
+
 let calculateCombatLevel = function (skills) { /// formula from http://runescape.wikia.com/wiki/Combat_level
   return Math.floor((1.3 * Math.max((skills.attack.level + skills.strength.level), 2 * skills.magic.level, 2*skills.ranged.level) + skills.defence.level + skills.hitpoints.level + Math.floor(skills.prayer.level / 2) + Math.floor(skills.summoning.level / 2)) / 4)
 };
 
-Promise.all([itemRef.once("value"), rsapi.rs.hiscores.player("bradorego")])
-  .then(([itemSnapshot, user]) => {
-    let items = itemSnapshot.val();
-    console.log(items.length);
+let getFilteredItems = function (username = "bradorego", res, next) {
+  console.log(username);
+  Promise.all([itemRef.once("value"), rsapi.rs.hiscores.player(username)])
+    .then(([itemSnapshot, user]) => {
+      let items = itemSnapshot.val();
+      console.log(items.length);
 
-    user.skills.combat = {"level": (calculateCombatLevel(user.skills))};
-    let filtered = items.filter((item) => {
-      if (!item.skills) {
-        return true;
-      }
-      let goodToGo = true;
-      item.skills.forEach((skill) => {
-        if (skill.type === "constitution") {
-          skill.type = "hitpoints"; /// i hate you rsapi
+      user.skills.combat = {"level": (calculateCombatLevel(user.skills))};
+      let filtered = items.filter((item) => {
+        if (!item.skills) {
+          return true;
         }
-        // console.log(skill.level, user.skills[skill.type].level);
-        if (skill.level > user.skills[skill.type].level) {
-          goodToGo = false;
-          return false;
-        }
+        let goodToGo = true;
+        item.skills.forEach((skill) => {
+          if (skill.type === "constitution") {
+            skill.type = "hitpoints"; /// i hate you rsapi
+          }
+          // console.log(skill.level, user.skills[skill.type].level);
+          if (skill.level > user.skills[skill.type].level) {
+            goodToGo = false;
+            return false;
+          }
+        });
+        return goodToGo;
       });
-      return goodToGo;
+
+      console.log(filtered.length);
+      res.json(filtered);
+      firebase.database().goOffline(); /// tell FB to release so node process ends
+    }).catch((err) => {
+      console.error(err);
+      res.status(err.statusCode).send(err);
     });
+  };
 
-    console.log(filtered.length);
-    firebase.database().goOffline(); /// tell FB to release so node process ends
-  });
+app.get("/user/:user", function (req, res, next) {
+  getFilteredItems(req.params.user, res);
+});
 
-
+process.env.TZ = "America/Chicago";
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
